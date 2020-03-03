abc27fa3cbc2843b37a4c13d2232e260d14f0e86
uesmgn <ug.0114@icloud.com>
2020-03-03 22:51:48 +0900

    Initial commit

---
+++ detchar/dataset.py
@@ -0,0 +1,26 @@
+'''
+
+'''
+import torch
+from torch import nn
+from torch.utils import data
+from PIL import Image
+
+class Dataset(data.Dataset):
+    def __init__(self, df, transform=None):
+        self.df = df
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        (label, path) = self.df.iloc[idx,:]
+        img = Image.open(path)
+        if self.transform:
+            img = self.transform(img)
+        return (img, label)
+
+    def get_labels(self, dtype=str):
+        labels = self.df.iloc[:,0].unique().astype(dtype)
+        return sorted(labels)
