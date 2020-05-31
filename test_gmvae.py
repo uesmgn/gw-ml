987a36b824111c7b17cb120c26d5b773ad4c910a
uesmgn <ug.0114@icloud.com>
2020-05-31 21:35:34 +0900

    Update some files

--- test_gmvae.py
+++ test_gmvae.py
@@ -23,24 +23,24 @@ parser = argparse.ArgumentParser(
     description='PyTorch Implementation of GMVAE Clustering')
 
 # NN Architecture
-parser.add_argument('-y', '--y_dim', type=int, default=10,
-                    help='number of classes (default: 10)')
-parser.add_argument('-z', '--z_dim', default=512, type=int,
-                    help='gaussian size (default: 512)')
-parser.add_argument('-w', '--w_dim', default=20, type=int,
-                    help='w dim (default: 20)')
-parser.add_argument('-e', '--n_epoch', default=1000, type=int,
-                    help='number of epochs (default: 1000)')
-parser.add_argument('-b', '--batch_size', default=32, type=int,
-                    help='batch size (default: 32)')
-parser.add_argument('-s', '--sigma', default=1., type=float,
-                    help='sigma of reconstruction_loss (default: 1.)')
-parser.add_argument('-n', '--num_workers', default=4, type=int,
-                    help='num_workers of DataLoader (default: 4)')
-parser.add_argument('-i', '--eval_itvl', default=5, type=int,
-                    help='eval interval (default: 5)')
-parser.add_argument('-lr', '--lr', default=0.001, type=float,
-                    help='learning rate (default: 0.001)')
+parser.add_argument('-y', '--y_dim', type=int,
+                    help='number of classes')
+parser.add_argument('-z', '--z_dim', type=int,
+                    help='gaussian size')
+parser.add_argument('-w', '--w_dim', type=int,
+                    help='w dim')
+parser.add_argument('-e', '--n_epoch', type=int,
+                    help='number of epochs')
+parser.add_argument('-b', '--batch_size', type=int,
+                    help='batch size')
+parser.add_argument('-s', '--sigma', type=float,
+                    help='sigma of reconstruction_loss')
+parser.add_argument('-n', '--num_workers', type=int,
+                    help='num_workers of DataLoader')
+parser.add_argument('-i', '--eval_itvl', type=int,
+                    help='eval interval')
+parser.add_argument('-lr', '--lr', type=float,
+                    help='learning rate')
 args = parser.parse_args()
 
 def get_loss(params, sigma=1.):
@@ -84,20 +84,19 @@ if __name__ == '__main__':
 
     # test params
     x_shape = (1, 486, 486)
-    y_dim = args.y_dim
-    z_dim = args.z_dim
-    w_dim = args.w_dim
-
-    sigma = args.sigma
+    y_dim = args.y_dim or ini.getint('net', 'y_dim')
+    z_dim = args.z_dim or ini.getint('net', 'z_dim')
+    w_dim = args.w_dim or ini.getint('net', 'w_dim')
 
     device_ids = range(torch.cuda.device_count())
     device = f'cuda:{device_ids[0]}' if torch.cuda.is_available() else 'cpu'
-    n_epoch = args.n_epoch
-    batch_size = args.batch_size
-    num_workers = args.num_workers
+    n_epoch = args.n_epoch or ini.getfloat('conf', 'n_epoch')
+    batch_size = args.batch_size or ini.getfloat('conf', 'batch_size')
+    num_workers = args.num_workers or ini.getfloat('conf', 'num_workers')
+    sigma = args.sigma or ini.getfloat('conf', 'sigma')
+    eval_itvl = args.eval_itvl or ini.getfloat('conf', 'eval_itvl')
+    lr = args.lr or ini.getfloat('conf', 'lr')
 
-    eval_itvl = args.eval_itvl
-    lr = args.lr
     outdir = 'result_gmvae'
     if not os.path.exists(outdir):
         os.mkdir(outdir)
