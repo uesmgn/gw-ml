d3978a9624f85107b7c07a77176992131dea498e
uesmgn <ug.0114@icloud.com>
2020-06-02 00:50:54 +0900

    Update loss.py

--- gmvae/loss.py
+++ gmvae/loss.py
@@ -12,7 +12,7 @@ def reconstruction_loss(x, x_, sigma=1.):
 
 
 def conditional_kl_loss(z_x, z_x_mean, z_x_var,
-                        z_wys, z_wy_means, z_wy_vars, y_wz):
+                        z_wy, z_wy_means, z_wy_vars, y_wz):
     # Conditional loss
     # q(z|x)=N(μ_x,σ_x)
     # logp = −0.5 * { log(det(σ_x^2)) + (z − μ_x)^2 / σ_x^2 }
@@ -21,11 +21,11 @@ def conditional_kl_loss(z_x, z_x_mean, z_x_var,
     eps = 1e-6
     logq = -0.5 * (torch.log(z_x_var + eps)
                    + torch.pow(z_x - z_x_mean, 2) / z_x_var).sum(1)
-    # K = z_wy_means.shape[-1]
-    # z_wy = z_x.repeat(1, K).view(
-    #     z_x.shape[0], K, -1).transpose(1, 2)  # (batch_size, z_dim, K)
+    K = y_wz.shape[-1]
+    z_wy = z_wy.repeat(1, K).view(
+        z_wy.shape[0], K, -1).transpose(1, 2)  # (batch_size, z_dim, K)
     logp = -0.5 * (y_wz * torch.log(z_wy_vars + eps).sum(1)
-                   + y_wz * (torch.pow(z_wys - z_wy_means, 2) / z_wy_vars).sum(1)).sum(1)
+                   + y_wz * (torch.pow(z_wy - z_wy_means, 2) / z_wy_vars).sum(1)).sum(1)
     kl = logq - logp
     return kl
 
