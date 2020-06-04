5c1dd2a7da573cdfe5ebcdb3ad69a247c593739b
uesmgn <ug.0114@icloud.com>
2020-06-04 14:25:37 +0900

    Update loss.py

--- gmvae/loss.py
+++ gmvae/loss.py
@@ -6,9 +6,7 @@ import numpy as np
 def reconstruction_loss(x, x_, sigma=1.):
     # Reconstruction loss
     # 1/2σ * Σ(x - x_)**2
-    loss = 0.5 / sigma * F.mse_loss(x_, x, reduction='none')
-    # loss = F.binary_cross_entropy(x_, x, reduction='none')
-    loss = loss.sum(-1).sum(-1)
+    loss = 0.5 / sigma * F.mse_loss(x_, x, reduction='mean')
     return loss
 
 
@@ -27,21 +25,28 @@ def conditional_kl(z_x, z_x_mean, z_x_var,
         z_x.shape[0], K, -1).transpose(1, 2)  # (batch_size, z_dim, K)
     logp = -0.5 * (y_wz * torch.log(z_wy_vars + eps).sum(1)
                    + y_wz * (torch.pow(z_wy - z_wy_means, 2) / z_wy_vars).sum(1)).sum(1)
-    kl = logq - logp
-    return kl
+    kl = (logq - logp).mean()
+    return -kl
 
 
 def w_prior_kl(w_mean, w_var):
+    # input: μ_θ(w), (batch_size, w_dim)
+    # input: σ_θ(w), (batch_size, w_dim)
     eps = 1e-10
     kl = 0.5 * (w_var - 1 - torch.log(w_var + eps) +
                 torch.pow(w_mean, 2)).sum(-1)
-    return kl
+    kl = kl.mean()
+    return kl.mean()
 
 
-def y_prior_kl(y_wz, thres=1.):
+def y_prior_kl(y_wz, thres=1.5):
+    # input: p_θ(y=1|w,z), (batch_size, K)
+    # KL(p_θ(y=1|w,z)||p(y)) = Σ{p(y=1)log[p_θ(y=1|w,z)/p(y=1)]} < 0
+    k = y_wz.shape[-1]
     eps = 1e-10
-    k = y_wz.shape[1]
-    kl = -np.log(k) - 1 / k * torch.log(y_wz + eps).sum(1)
-    # kl = (y_wz * (torch.log(y_wz + eps) + np.log(k))).sum(1)
-    kl = torch.max(kl, torch.ones_like(kl)*thres)
+    pi = 1 / k
+    kl = (pi * torch.log(y_wz / pi + eps)).sum(-1)
+    # output is E_q[KL]
+    kl = kl.mean() # negative value minimize(kl)
+    kl = torch.max(kl, torch.ones_like(kl) * thres)
     return kl
