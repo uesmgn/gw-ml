d9cc2eac6d1c2b5ba09725a12ae39db4d02cc4e1
uesmgn <ug.0114@icloud.com>
2020-06-04 15:17:49 +0900

    Update network.py

--- gmvae/network.py
+++ gmvae/network.py
@@ -107,11 +107,11 @@ class Gaussian(nn.Module):
         x = self.dense(x)
         mean, logit = torch.split(x, x.shape[1] // 2, 1)
         var = F.softplus(logit) + 1e-8
-        if self.training:
-            x = ut.reparameterize(mean, var)
-        else:
-            x = mean
-        # x = ut.reparameterize(mean, var)
+        # if self.training:
+        #     x = ut.reparameterize(mean, var)
+        # else:
+        #     x = mean
+        x = ut.reparameterize(mean, var)
         return x, mean, var
 
 
