82d369e80affd4d5d3175bada3109683f58d678b
uesmgn <ug.0114@icloud.com>
2020-06-04 23:54:55 +0900

    Update loss.py

--- gmvae/loss.py
+++ gmvae/loss.py
@@ -11,7 +11,7 @@ def reconstruction_loss(x, x_, sigma=1.):
     return loss
 
 
-def conditional_kl(z_x, z_x_mean, z_x_var,
+def conditional_negative_kl(z_x, z_x_mean, z_x_var,
                    z_wy_means, z_wy_vars, y_wz):
     # Conditional loss
     # q(z|x)=N(μ_x,σ_x)
@@ -27,7 +27,7 @@ def conditional_kl(z_x, z_x_mean, z_x_var,
     logp = -0.5 * (y_wz * torch.log(z_wy_vars + eps).sum(1)
                    + y_wz * (torch.pow(z_wy - z_wy_means, 2) / z_wy_vars).sum(1)).sum(1)
     kl = (logq - logp).mean()
-    return kl
+    return -kl
 
 
 def w_prior_kl(w_mean, w_var):
