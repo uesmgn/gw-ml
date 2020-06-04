9e36fb891ec9ae4f786a9f0586f4b6663ed64bac
uesmgn <ug.0114@icloud.com>
2020-06-04 16:01:17 +0900

    Update run_gmvae.py

--- run_gmvae.py
+++ run_gmvae.py
@@ -66,12 +66,12 @@ def get_loss(params, args):
     y_thres = args.get('y_thres') or 1.
 
     rec_loss = loss.reconstruction_loss(x, x_z, sigma)
-    conditional_kl = loss.conditional_kl(z_x, z_x_mean, z_x_var,
+    conditional_kl = loss.conditional_negative_kl(z_x, z_x_mean, z_x_var,
                                          z_wy_means, z_wy_vars,
                                          y_wz)
-    w_prior_kl = loss.w_prior_kl(w_x_mean, w_x_var)
-    y_prior_kl = loss.y_prior_kl(y_wz, y_thres)
-    total = -(rec_loss - conditional_kl - w_prior_kl - y_prior_kl)
+    w_prior_kl = loss.w_prior_negative_kl(w_x_mean, w_x_var)
+    y_prior_kl = loss.y_prior_negative_kl(y_wz, y_thres)
+    total = rec_loss - conditional_kl - w_prior_kl - y_prior_kl
     return total, {'rec_loss': rec_loss,
                    'conditional_kl': conditional_kl,
                    'w_prior_kl': w_prior_kl,
