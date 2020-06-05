07b4e1c661fd742162b9bf9be9203a9e66367967
uesmgn <ug.0114@icloud.com>
2020-06-06 00:11:28 +0900

    Update loss.py

--- gmvae/loss.py
+++ gmvae/loss.py
@@ -15,7 +15,7 @@ def reconstruction_loss(x, x_, type='bce', sigma=1.):
     return loss
 
 
-def conditional_kl(z_x, z_x_mean, z_x_var,
+def conditional_negative_kl(z_x, z_x_mean, z_x_var,
                    z_wy_means, z_wy_vars, y_wz):
     # Conditional loss
     # q(z|x)=N(μ_x,σ_x)
@@ -26,10 +26,9 @@ def conditional_kl(z_x, z_x_mean, z_x_var,
     logq = -0.5 * (torch.log(z_x_var + eps)
                    + torch.pow(z_x - z_x_mean, 2) / z_x_var).sum(1)
     K = y_wz.shape[-1]
-    z_wy = z_x.repeat(1, K).view(
-        z_x.shape[0], K, -1).transpose(1, 2)  # (batch_size, z_dim, K)
-    logp = -0.5 * (y_wz * torch.log(z_wy_vars + eps).sum(1)
-                   + y_wz * (torch.pow(z_wy - z_wy_means, 2) / z_wy_vars).sum(1)).sum(1)
+    z_wy = z_x.repeat(1, K).view(z_x.shape[0], K, -1)  # (batch_size, K, z_dim)
+    logp = -0.5 * (y_wz * torch.log(z_wy_vars + eps).sum(-1)
+                   + y_wz * (torch.pow(z_wy - z_wy_means, 2) / z_wy_vars).sum(-1)).sum(-1)
     kl = (logq - logp).mean()
     return kl
 
@@ -41,10 +40,10 @@ def gaussian_negative_kl(mean, var):
     kl = 0.5 * (1 + torch.log(var) -
                 torch.pow(mean, 2) - var).sum(-1)
     kl = kl.mean()
-    return kl.mean()
+    return kl
 
 
-def y_prior_kl(y_wz, thres=1.5):
+def y_prior_negative_kl(y_wz, thres=1.5):
     # input: p_θ(y=1|w,z), (batch_size, K)
     # KL(p_θ(y=1|w,z)||p(y)) = Σ{p(y=1)log[p_θ(y=1|w,z)/p(y=1)]} < 0
     k = y_wz.shape[-1]
@@ -54,4 +53,4 @@ def y_prior_kl(y_wz, thres=1.5):
     # output is E_q[KL]
     kl = kl.mean() # negative value minimize(kl)
     # kl = torch.max(kl, torch.ones_like(kl) * thres)
-    return -kl
+    return kl
