74eb412bf5372df6b2bc1c8eedc8d9b0535e47b7
uesmgn <ug.0114@icloud.com>
2020-06-07 13:41:08 +0900

    Update network.py

--- gmvae/network.py
+++ gmvae/network.py
@@ -529,7 +529,7 @@ class GMVAE(nn.Module):
                     nn.init.constant_(m.bias, 0)
 
 
-    def forward(self, x, return_loss=False):
+    def forward(self, x, return_params=False):
         # Encoder
         h = self.zw_x_graph(x) # (batch_size, 1, 486, 486) -> 100*6*6
         # x -> Encoder -> Decoder -> x'
@@ -537,7 +537,6 @@ class GMVAE(nn.Module):
         w_x, w_x_mean, w_x_var = self.w_x_graph(h) # (batch_size, 100*6*6) -> w_dim
         y_wz = self.y_wz_graph(torch.cat((w_x, z_x), 1)) # z_dim+w_dim -> y_dim
         x_z = self.x_z_graph(z_x)
-
         # Decoder
         z_wys_stack = []
         z_wy_means_stack = []
@@ -554,17 +553,17 @@ class GMVAE(nn.Module):
         _, p = torch.max(y_wz, dim=1)  # (batch_size, )
         z_wy = z_wys[torch.arange(z_wys.shape[0]), :, p]  # (batch_size, z_dim)
 
-        recon_loss = self.rec_wei * loss.reconstruction_loss(x, x_z)
-        cond_kl = self.cond_wei * loss.conditional_negative_kl(z_x, z_x_mean, z_x_var,
-                                                z_wy_means, z_wy_vars, y_wz)
-        # maximize w-prior term
-        gauss_kl = self.w_wei * loss.gaussian_negative_kl(w_x_mean, w_x_var)
-        # maximize y-prior term
-        y_kl = self.y_wei * loss.y_prior_negative_kl(y_wz)
-        total = recon_loss - cond_kl - gauss_kl - y_kl
-
-        if return_loss:
-            return total
+        if return_params:
+            return {'x': x,
+                    'z_x': z_x, 'z_x_mean': z_x_mean, 'z_x_var': z_x_var,
+                    'w_x': w_x, 'w_x_mean': w_x_mean, 'w_x_var': w_x_var,
+                    'y_wz': y_wz,
+                    'y_pred': p,
+                    'z_wy': z_wy,  # (batch_size, z_dim, K)
+                    'z_wys': z_wys,
+                    'z_wy_means': z_wy_means,
+                    'z_wy_vars': z_wy_vars,
+                    'x_z': x_z}
         else:
             return x_z
 
