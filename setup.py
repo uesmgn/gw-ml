cd55aa3af6d17d1455c8221d779050c6aa179210
uesmgn <ug.0114@icloud.com>
2020-06-08 06:46:42 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+import json
+import configparser
+from glob import glob
+from collections import defaultdict
+import re
+import pandas as pd
+
+
+def init_dataset():
+
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    ini = configparser.ConfigParser()
+    ini.read(f'{base_dir}/config.ini', 'utf-8')
+
+    dataset_dir = ini.get('dataset','dataset_dir')
+    filter_str = ini.get('dataset','filter_str') or '*_1.0.png'
+    subsets = json.loads(ini.get('dataset', 'subsets')) or ['test', 'train', 'validation']
+
+    dataset_path = f'{base_dir}/{DATASET_DIR}'
+    assert os.path.exists(dataset_path)
+
+    data = defaultdict()
+
+    for subset in subsets:
+        labels = [os.path.basename(p) for p in glob(f'{dataset_path}/{subset}/*')]
+        for label in labels:
+            paths = glob(f'{dataset_path}/{subset}/{label}/{filter_str}')
+            for path in paths:
+                idx = re.match(r'.+(L1|H1)_([a-zA-Z0-9]+)_.+', path).group(2)
+                data[idx] = {'label': label, 'path': path}
+
+    df = pd.DataFrame.from_dict(data, orient='index')
+    df.to_json(f'{dataset_dir}.json')
+    print(f'Success to make .json file have columns (label, path) , shape {df.shape}.')
