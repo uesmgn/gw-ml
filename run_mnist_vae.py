52870922fded39905707242fd2627b5f22b5b3d1
uesmgn <ug.0114@icloud.com>
2020-06-10 17:09:30 +0900

    Update run_mnist_vae.py

--- run_mnist_vae.py
+++ run_mnist_vae.py
@@ -113,8 +113,6 @@ if __name__ == '__main__':
                     z_x = torch.cat((z_x, params['z_x']), 0)
                     labels_true = torch.cat([labels_true, l])
                     n_samples += x.shape[0]
-                    if batch > 50:
-                        break
                 print(f'test epoch {epoch + 1}, n_samples {n_samples}')
 
                 # decompose...
