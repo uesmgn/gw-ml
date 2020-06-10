0c049f7acb4b7925b4083b7c48a0f9e26136f84b
uesmgn <ug.0114@icloud.com>
2020-06-10 15:00:25 +0900

    Update loss_function.py

--- vae/loss_function.py
+++ vae/loss_function.py
@@ -6,72 +6,22 @@ class Criterion:
     def __init__(self):
         pass
 
-    def gmvae_loss(self, params, weights, reduction='none'):
+    def vae_loss(self, params, reduction='none'):
         # get parameters from model
         x = params['x']
         x_z = params['x_z']
-        w_x_mean, w_x_var = params['w_x_mean'], params['w_x_var']
-        y_wz = params['y_wz']
         z_x = params['z_x']
         z_x_mean, z_x_var = params['z_x_mean'], params['z_x_var'],
-        z_wy_means, z_wy_vars = params['z_wy_means'], params['z_wy_vars']
 
-        # get loss weights from arguments
-        rec_wei = weights.get('rec_wei') or 1.
-        cond_wei = weights.get('cond_wei') or 1.
-        w_wei = weights.get('w_wei') or 1.
-        y_wei = weights.get('y_wei') or 1.
-        y_thres = weights.get('y_thres') or 0.
-
-        rec_loss = rec_wei * self.binary_cross_entropy(x, x_z)
-        conditional_negative_kl = \
-            cond_wei * self.conditional_negative_kl(z_x, z_x_mean, z_x_var,
-                                                    z_wy_means, z_wy_vars, y_wz)
-        w_prior_negative_kl = w_wei * self.gaussian_negative_kl(w_x_mean, w_x_var)
-        y_prior_negative_kl = y_wei * self.y_prior_negative_kl(y_wz, thres=y_thres)
+        rec_loss = self.binary_cross_entropy(x, x_z)
+        gaussian_negative_kl = self.gaussian_negative_kl(z_x_mean, z_x_var)
 
         total = torch.cat([rec_loss.view(-1),
-                           conditional_negative_kl.view(-1),
-                           w_prior_negative_kl.view(-1),
-                           y_prior_negative_kl.view(-1)])
+                           gaussian_negative_kl.view(-1)])
 
         if reduction is 'sum':
             return total.sum()
         return total
-
-    def gmvae_m2_loss(self, params, weights, reduction='none'):
-        x = params['x']
-        x_z = params['x_z']
-        z_y_mean, z_y_var = params['z_y_mean'], params['z_y_var']
-        y_x = params['y_x']
-        y_x_prob = params['y_x_prob']
-        z_xy = params['z_xy']
-        z_xy_mean, z_xy_var = params['z_xy_mean'], params['z_xy_var'],
-
-        # get loss weights from arguments
-        rec_wei = weights.get('rec_wei') or 1.
-        cond_wei = weights.get('cond_wei') or 1.
-        y_wei = weights.get('y_wei') or 1.
-        y_thres = weights.get('y_thres') or 0.
-
-        rec_loss = rec_wei * self.binary_cross_entropy(x, x_z)
-        conditional_negative_kl = \
-            cond_wei * self.gaussian_loss(z_xy, z_xy_mean, z_xy_var,
-                                          z_y_mean, z_y_var)
-        y_prior_negative_kl = y_wei * self.y_prior_negative_kl(y_x_prob, thres=y_thres)
-
-        total = torch.cat([rec_loss.view(-1),
-                           conditional_negative_kl.view(-1),
-                           y_prior_negative_kl.view(-1)])
-
-        if reduction is 'sum':
-            return total.sum()
-        return total
-
-    def cross_entropy(self, output, target):
-        loss = F.cross_entropy(output, target, reduction='sum')
-        loss /= output.shape[0]
-        return loss
 
     def binary_cross_entropy(self, x, x_):
         # Reconstruction loss
@@ -84,47 +34,10 @@ class Criterion:
         loss /= x.shape[0]
         return loss
 
-
-    def conditional_negative_kl(self, z_x, z_x_mean, z_x_var,
-                                z_wy_means, z_wy_vars, y_wz):
-        # Conditional loss
-        # q(z|x)=N(μ_x,σ_x)
-        # logp = −0.5 * { log(det(σ_x^2)) + (z − μ_x)^2 / σ_x^2 }
-        # q(z|w,y=1)=N(μ_w,σ_w)
-        # logq = −0.5 * { Σπlog(det(σ_w^2)) + Σπ(z − μ_w)^2 / σ_w^2 }
-        eps = 1e-10
-        logq = -0.5 * (torch.log(z_x_var + eps).sum(1)
-                      + (torch.pow(z_x - z_x_mean, 2) / z_x_var).sum(1))
-        K = y_wz.shape[-1]
-        z_wy = z_x.repeat(1, K).view(z_x.shape[0], K, -1).transpose(1,2)  # (batch_size, z_dim, K)
-        logp = -0.5 * (y_wz * torch.log(z_wy_vars + eps).sum(1)
-                       + y_wz * (torch.pow(z_wy - z_wy_means, 2) / z_wy_vars).sum(1)).sum(1)
-        kl = (logq - logp).mean()
-        return kl
-
-    def gaussian_loss(self, z_xy, z_xy_mean, z_xy_var,
-                      z_y_mean, z_y_var):
-        eps = 1e-10
-        logq = -0.5 * (torch.log(z_xy_var + eps).sum(1)
-                      + (torch.pow(z_xy - z_xy_mean, 2) / z_xy_var).sum(1))
-        logp = -0.5 * (torch.log(z_y_var + eps).sum(1)
-                      + (torch.pow(z_xy - z_y_mean, 2) / z_y_var).sum(1))
-        kl = (logq - logp).mean()
-        return kl
-
     def gaussian_negative_kl(self, mean, var):
         # input: μ_θ(w), (batch_size, w_dim)
         # input: σ_θ(w), (batch_size, w_dim)
         eps = 1e-10
         kl = 0.5 * (var - 1 - torch.log(var) + torch.pow(mean, 2)).sum(-1)
         kl = kl.mean()
-        return kl
-
-    def y_prior_negative_kl(self, y_wz, pi=None, thres=0.):
-        eps = 1e-10
-        k = y_wz.shape[-1]
-        kl = -np.log(k) - 1 / k * torch.log(y_wz + eps).sum(-1)
-        thres = torch.ones_like(kl) * thres
-        kl = torch.max(kl, thres)
-        kl = kl.mean() # negative value minimize(kl)
         return kl
