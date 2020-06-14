9bb1e44c486952f16ecb1cb13d54003aea741e05
uesmgn <ug.0114@icloud.com>
2020-06-14 20:51:21 +0900

    Update decomposition.py

--- utils/clustering/decomposition.py
+++ utils/clustering/decomposition.py
@@ -1,6 +1,6 @@
 from sklearn.manifold import TSNE as NormalTSNE
 from MulticoreTSNE import MulticoreTSNE
-from tsnecuda import TSNE as CudaTSNE
+# from tsnecuda import TSNE as CudaTSNE
 import torch
 
 
@@ -10,17 +10,12 @@ __all__ = [
 
 
 class TSNE:
-    def __init__(self, n_components=2, cuda=False, multi_core=False, n_jobs=4):
-        if cuda:
-            assert torch.cuda.is_available()
-            # cudatsne only support n_components=2
-            self.tsne = CudaTSNE(n_components=2)
+    def __init__(self, n_components=2, multi_core=True, n_jobs=4):
+        if multi_core:
+            self.tsne = MulticoreTSNE(
+                n_jobs=n_jobs, n_components=n_components)
         else:
-            if multi_core:
-                self.tsne = MulticoreTSNE(
-                    n_jobs=n_jobs, n_components=n_components)
-            else:
-                self.tsne = NormalTSNE(n_components=n_components)
+            self.tsne = NormalTSNE(n_components=n_components)
 
     def fit_transform(self, data):
         data = self.tsne.fit_transform(data)
