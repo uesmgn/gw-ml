808934ef29fcd5c6382dd984fda695ddef64f1e1
uesmgn <ug.0114@icloud.com>
2020-06-15 11:01:07 +0900

    Update metrics.py

--- utils/clustering/metrics.py
+++ utils/clustering/metrics.py
@@ -14,11 +14,11 @@ __all__ = [
 # Confution matrix between 2 results have different labels
 def confusion_matrix(true, pred, labels_true=None, labels_pred=None,
                      normalize=True, return_labels=False):
-    true, pred = _check_array(true, pred, check_size=True)
+    true, pred = check_array(true, pred, check_size=True)
     if labels_true is None:
-        labels_true = _check_array(true, sort=True, unique=True)
+        labels_true = check_array(true, sort=True, unique=True)
     if labels_pred is None:
-        labels_pred = _check_array(pred, sort=True, unique=True)
+        labels_pred = check_array(pred, sort=True, unique=True)
     matrix = np.zeros([len(labels_true), len(labels_pred)])
     for (t, p) in itertools.product(labels_true, labels_pred):
         idx = np.where(pred[np.where(true==t)]==p)[0]
@@ -33,11 +33,11 @@ def confusion_matrix(true, pred, labels_true=None, labels_pred=None,
 
 # Normalized Mutual Information Score
 def nmi(true, pred):
-    true, pred = _check_array(true, pred, check_size=True)
+    true, pred = check_array(true, pred, check_size=True)
     return metrics.adjusted_mutual_info_score(true, pred)
 
 
 # Adjusted Rand Index
 def ari(true, pred):
-    true, pred = _check_array(true, pred, check_size=True)
+    true, pred = check_array(true, pred, check_size=True)
     return metrics.adjusted_rand_score(true, pred)
