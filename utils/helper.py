3d88fe7264ad659040e4b1d12f42cf8c964de132
uesmgn <ug.0114@icloud.com>
2020-06-15 11:07:51 +0900

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -64,8 +64,8 @@ def _cast_array(arg):
     return arr.astype(cast_type) if cast_type else arr
 
 def acronym(name):
-    name = re.sub(r'(^[0-9a-zA-Z]{5,}(?=_?))|((?<=_)[0-9a-zA-Z]{5,})',
-                  lambda m: str(m.group(1) or '')[:3] + str(m.group(2) or '')[:1],
+    name = re.sub(r'(^[0-9a-zA-Z]{5,}(?=_?))|((?<=_)[0-9a-zA-Z]*)',
+                  lambda m: str(m.group(1) or '')[:5] + str(m.group(2) or '')[:1],
                   name)
     name = name.replace('_', '.')
     return name
