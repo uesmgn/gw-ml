37c86a5b89fd95a497b0d400844834a599014bf2
uesmgn <ug.0114@icloud.com>
2020-06-16 10:19:09 +0900

    Update criterion.py

--- net/criterion.py
+++ net/criterion.py
@@ -16,7 +16,7 @@ def gmvae_loss(params, beta=1.0):
     y_wz, pi, y_pred = params['y_wz'], params['pi'], params['y_pred']
 
     rec_loss = binary_cross_entropy(x_z, x)
-    cond_kl = gaussian_gmm_kl(z_x_mean, z_x_var,
+    cond_kl = gaussian_gmm_kl(z_x, z_x_mean, z_x_var,
                               z_wy_means, z_wy_vars, pi)
     w_kl = standard_gaussian_kl(w_x_mean, w_x_var)
     y_kl = uniform_categorical_kl(pi)
@@ -29,7 +29,7 @@ def gmvae_loss(params, beta=1.0):
                         w_kl,
                         y_kl], 1).detach()
 
-    return total.sum(0), each.sum(0)
+    return total, each
 
 def binary_cross_entropy(input, target):
     # x: (batch_size, x_size, x_size)
@@ -38,43 +38,39 @@ def binary_cross_entropy(input, target):
     assert input.shape == target.shape
     input = input.view(input.shape[0], -1)
     target = target.view(target.shape[0], -1)
-    loss = F.binary_cross_entropy(input, target, reduction='none')
-    return loss.sum(1)
+    loss = F.binary_cross_entropy(input, target, reduction='none').sum(1)
+    return loss.mean()
 
-def gaussian_gmm_kl(mean, var, means, variances, pi):
+def gaussian_gmm_kl(z, mean, variance, means, variances, pi):
     # mean: (batch_size, dim)
     # var: (batch_size, dim) > 0
     # means: (batch_size, dim, K)
     # vars: (batch_size, dim, K) > 0
     # pi: (batch_size, K)
     # kl: (batch_size, )
-    K = pi.shape[-1]
-    mean_repeat = mean.unsqueeze(-1).repeat(1, 1, K)
-    var_repeat = var.unsqueeze(-1).repeat(1, 1, K)
-    kl = (pi * gaussian_kl(mean_repeat, var_repeat, means, variances))
-    return kl.mean(1)
-
-def gaussian_kl(mean1, var1, mean2, var2):
-    # mean1: (batch_size, dim, .. )
-    # mean2: (batch_size, dim, .. )
-    # var1: (batch_size, dim, .. ) > 0
-    # var2: (batch_size, dim, .. ) > 0
-    # kl: (batch_size, .. )
-    assert (torch.cat([var1, var2]) > 0).all()
-    kl = 0.5 * (torch.log(var2 / var1) + var1 / var2 + torch.pow(mean1 - mean2, 2) / var2 - 1)
-    return kl.sum(1)
+    k = pi.shape[-1]
+    z_repeat = z.unsqueeze(-1).repeat(1, 1, k)
+    log_q = -0.5 * (torch.log(variance) + torch.pow(z - mean, 2) / variance ).sum(-1)
+    log_p = -0.5 * (
+        (pi * torch.log(variances).sum(1)).sum(-1) + \
+        (pi * (torch.pow(z_repeat - means, 2) / variances).sum(1)).sum(-1)
+    )
+    kl = log_q - log_p
+    return kl.mean()
 
 def standard_gaussian_kl(mean, var):
     # mean: (batch_size, dim, ..)
     # var: (batch_size, dim, ..)
     # kl: (batch_size, ..)
-    kl = 0.5 * (var - 1 - torch.log(var) + torch.pow(mean, 2))
-    return kl.sum(1)
+    kl = 0.5 * (var - 1 - torch.log(var) + torch.pow(mean, 2)).sum(1)
+    # sum over dimenntion of w
+    return kl.mean()
 
 def uniform_categorical_kl(y):
     # y: (batch_size, K)
     # kl: (batch_size, )
     k = y.shape[-1]
     u = torch.ones_like(y) / k
-    kl = F.kl_div(torch.log(u), y, reduction='none')
-    return kl.sum(1)
+    # (y * torch.log(y/u)).sum() = F.kl_div(torch.log(u), y, reduction='none')
+    kl = F.kl_div(torch.log(u), y, reduction='none').sum(1)
+    return kl.mean()
