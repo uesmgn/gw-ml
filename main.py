1a15fe42a493fd9d9692a17c2282f8defe65c059
uesmgn <ug.0114@icloud.com>
2020-06-26 20:14:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 from glob import glob
 from collections import defaultdict
-import re
+import uuid
 import pandas as pd
 
-DATASET_DIR = '../TrainingSet'
+DATASET_DIR = 'TrainingSet'
 
 if __name__ == '__main__':
-    base_dir = os.path.dirname(os.path.abspath(__file__))
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     dataset_path = f'{base_dir}/{DATASET_DIR}'
     labels = [os.path.basename(p) for p in glob(f'{dataset_path}/*')]
 
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         # paths = glob(f'{dataset_path}/{label}/*_1.0.png')
         paths = glob(f'{dataset_path}/{label}/*_1.0.png')
         for path in paths:
-            idx = re.match(r'.+(L1|H1)_([a-zA-Z0-9]+)_.+', path).group(2)
+            idx = uuid.uuid4()
             data[idx] = {'label': label, 'path': path}
     df = pd.DataFrame.from_dict(data, orient='index')
     df.to_json('dataset.json')
