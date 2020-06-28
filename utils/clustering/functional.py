8c8ab658883344b2340b9d0c1d3dd6b25af78794
uesmgn <ug.0114@icloud.com>
2020-06-28 19:04:08 +0900

    Update functional.py

--- utils/clustering/functional.py
+++ utils/clustering/functional.py
@@ -5,19 +5,24 @@ import faiss
 def run_kmeans(x, k):
     n_data, d = x.shape
 
-    clus = faiss.Clustering(d, k)
+    kmeans = faiss.Kmeans(d, k)
+    kmeans.seed = np.random.randint(1234)
+    kmeans.train(x)
+    _, I = kmeans.index.search(x, 1)
 
-    clus.seed = np.random.randint(1234)
-
-    clus.niter = 20
-    clus.max_points_per_centroid = 100
-    res = faiss.StandardGpuResources()
-    flat_config = faiss.GpuIndexFlatConfig()
-    flat_config.useFloat16 = False
-    flat_config.device = 0
-    index = faiss.GpuIndexFlatL2(res, d, flat_config)
-
-    clus.train(x, index)
-    _, I = index.search(x, 1)
+    # clus = faiss.Clustering(d, k)
+    #
+    # clus.seed = np.random.randint(1234)
+    #
+    # clus.niter = 20
+    # clus.max_points_per_centroid = 100
+    # res = faiss.StandardGpuResources()
+    # flat_config = faiss.GpuIndexFlatConfig()
+    # flat_config.useFloat16 = False
+    # flat_config.device = 0
+    # index = faiss.GpuIndexFlatL2(res, d, flat_config)
+    #
+    # clus.train(x, index)
+    # _, I = index.search(x, 1)
 
     return np.array([int(n[0]) for n in I])
