26bdeadb98a2d581fe3a5493e211dfa7fd9a3081
uesmgn <ug.0114@icloud.com>
2020-07-10 14:16:45 +0900

    test

--- utils/clustering/decomposition.py
+++ utils/clustering/decomposition.py
@@ -1,10 +1,11 @@
 from sklearn.manifold import TSNE as NormalTSNE
 # from MulticoreTSNE import MulticoreTSNE
+import faiss
 import torch
 
 
 __all__ = [
-    'TSNE'
+    'TSNE', 'PCA'
 ]
 
 
@@ -20,3 +21,12 @@ class TSNE:
     def fit_transform(self, data):
         data = self.tsne.fit_transform(data)
         return data
+
+class PCA:
+    def __init__(self, n_components=2):
+        self.n_components = n_components
+
+    def fit_transform(self, data):
+        mat = faiss.PCAMatrix(data.shape[-1], self.n_components)
+        mat.train(data)
+        return mat.apply_py(data)
